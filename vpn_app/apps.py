f9ee358deb674720461e244b9ece3e6138467469
Eugene Rabenko <qwer09846@gmail.com>
2024-01-24 12:03:20 +0200

    rename vpn app

--- vpn_app/apps.py
+++ vpn_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VpnServiceAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vpn_service_app'
+    name = 'vpn_app'
