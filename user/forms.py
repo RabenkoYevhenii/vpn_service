9d7adc26437d5b9310c3504177e97b1c942e0aee
Eugene Rabenko <qwer09846@gmail.com>
2024-01-24 16:48:51 +0200

    fix forms & views

--- user/forms.py
+++ user/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import (
     UserCreationForm,
     UserChangeForm,
@@ -7,9 +8,18 @@ from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = get_user_model()
-        fields = ["email", "password", "first_name", "last_name"]
+        fields = ("email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 
 class CustomUserChangeForm(UserChangeForm):
